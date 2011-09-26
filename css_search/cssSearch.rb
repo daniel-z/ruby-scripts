a40dfb7055e2c0bdb3adca40a1a600bb62890e26
Daniel Zamorano <dz@danielzamorano.com>
2011-09-26 17:41:45 -0500

    Improved logging and added license to the files

--- css_search/cssSearch.rb
+++ css_search/cssSearch.rb
@@ -1,16 +1,21 @@
 #!/usr/bin/ruby
 
+# This software by Daniel Zamorano is licensed under a 
+# Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+# Read the license here: http://creativecommons.org/licenses/by-nc-sa/3.0/
+
+# cssSearch - This searchs for a css property in a css file
+
 require 'css_parser'
 
 
 class CssSearch
-  
+
   def initialize( params )
-    @debug = params['debug'] || false
-    
+    @debug = params['debug'] || false    
     #look for needed parameters or exit
     if !params['cssFile']
-      logger 'initialize', 'Needed cssFile parameter'
+      logger ({"type" => 'error', "function" => 'initialize', "message" => 'Needed cssFile parameter'})
       exit
     end
     
@@ -19,7 +24,9 @@ class CssSearch
     @parserObj.load_file!(@params['cssFile'])
 
       @params.each_pair do |key,value|
-        logger 'initialize', "params: Key = #{key}, Value = #{value}"
+        logger ({"type"     => 'info', 
+                "function" => 'initialize', 
+                "message"  => "params: Key = #{key}, Value = #{value}"})
       end
 
   end
@@ -30,37 +37,77 @@ class CssSearch
     
     #look for needed parameters or exit
     if !params['cssPropertyMask']
-      logger functionName, 'Needed cssPropertyMask parameter'
+      logger ({"type"     => 'error',
+              "function" => "#{functionName}",
+              "message"  => "Needed cssPropertyMask parameter"})
       exit
     end
     
     cssPropertyMask = ( params['cssPropertyMask'] || @params["cssPropertyMask"] ) || '*'
     cssSelectors = Hash.new
     
-    logger functionName , "cssPropertyMask: #{cssPropertyMask}"
+    logger ({"type"     => 'info',
+            "function" => "#{functionName}", 
+            "message"  => "cssPropertyMask: #{cssPropertyMask}"})
     
     # iterate through selectors
     @parserObj.each_selector() do |selector, declarations, specificity|
         if  declarations =~ /#{cssPropertyMask}/ 
           #just set the selector as key and 1 to avoid duplicate elements if we use a hash
           cssSelectors["#{selector}"] = 1
-          
-          logger functionName , "true! #{selector} -> #{cssSelectors["#{selector}"]}"
+
+          logger ({"type"     => 'info',
+                  "function" => "#{functionName}", 
+                  "message"  => "true! #{selector} -> #{cssSelectors["#{selector}"]}"})
         end
     end
 
     # get selector into array
     resultingSelectors = Array.new(cssSelectors.keys)
 
-    logger functionName, "Resulting Selectors:"
-    resultingSelectors.each do | selector | logger functionName,  "#{selector}" end
+    logger ({"type"     => 'info',
+            "function" => "#{functionName}", 
+            "message"  => "Resulting Selectors:"})
+
+    resultingSelectors.each do | selector |
+      logger ({"type" => 'info', "function" => "#{functionName}", "message"  => "#{selector}"})     
+    end
 
     return resultingSelectors
 
-  end # getSelectors
+  end 
   
-  def logger (function, message)
-    puts "##{function}: #{message}" if @debug
+  
+  # ---
+  # private functions
+  # ---
+  
+  private 
+  
+  def logger (params)
+    # print message just if debug is enabled
+    exit if !@debug
+    
+    prefix = Hash.new
+    prefix['error']   = 'ERROR!!'
+    prefix['warning'] = 'WARNING'
+    prefix['info']    = 'INFO'    
+
+    #look for needed parameters or exit    
+    for parameter in ['message','type','function'].each
+      if !params["#{parameter}"]
+          puts "#{prefix['error']} - logger: needed #{parameter} parameter!"
+          exit
+      end
+    end
+    
+    if !prefix["#{params['type']}"] and @debug
+      puts "#{prefix['error']} - logger: invalid log type for message: #{params['message']}"
+      exit
+    end
+
+    puts "#{prefix["#{params['type']}"]} - #{params['function']}: #{params['message']}"
+
   end
 
 end #class
--- css_search/test/cssSearch_test.rb
+++ css_search/test/cssSearch_test.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/ruby
 
-# testing the cssParser
-# @Autor: Daniel Zamorano
+# This software by Daniel Zamorano is licensed under a 
+# Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+# Read the license here: http://creativecommons.org/licenses/by-nc-sa/3.0/
+
+# cssSearch_test - Testing the cssSearch module
 
 require '../cssSearch'
 require 'test/unit'
@@ -12,21 +15,20 @@ class TestCssSearch < Test::Unit::TestCase
   def setup
     @params = Hash.new()
     @params["cssFile"]    = "./resources/example.css"
-#    @params["cssPropertyMask"] = 'border-radius'
+    @params["cssPropertyMask"] = 'border-radius'
     @params["debug"] = true
-    
     @cssSearchObj = CssSearch.new(@params)
-    @results = Array.new() 
   end
 
   def test_getSelectors
     # results for the first test
-    @results[0] = "#id-with-property"
-    @results[1] = ".class-with-property"
-    @results[2] = "p.element-with-property"
-    @results[3] = "span"
+    results = Array.new() 
+    results[0] = "#id-with-property"
+    results[1] = ".class-with-property"
+    results[2] = "p.element-with-property"
+    results[3] = "span"
 
-    assert_equal @results, @cssSearchObj.getSelectors(@params), "getSelectors for border-radius test!"
+    assert_equal results, @cssSearchObj.getSelectors(@params), "getSelectors for border-radius test!"
   end
 
 end
