0baa732dd51f6a2ab653a702771dec0bb0638539
Daniel Zamorano <dz@danielzamorano.com>
2011-09-26 23:27:52 -0500

    Added GetAllSelectorsInDirectory functions and test

--- css_search/cssSearch.rb
+++ css_search/cssSearch.rb
@@ -17,9 +17,9 @@ class CssSearch
   end
 
   def getSelectors (params)
-    
+
     functionName = 'getSelectors'
-    
+
     #look for needed parameters or return
     for parameter in ['cssPropertyMask', 'cssFile']
       if !params["#{parameter}"]
@@ -29,53 +29,101 @@ class CssSearch
         return false
       end
     end
-    
+
     cssPropertyMask = ( params['cssPropertyMask'] ) || '*'
     cssSelectors = Hash.new
-  
+
     if isValidFile({"file" => params['cssFile']})
       @parserObj.load_file!(params['cssFile'])   
     end
-  
+
     logger ({"type"     => 'info',
             "function" => "#{functionName}", 
             "message"  => "cssPropertyMask: #{cssPropertyMask}"})
-    
+
     # iterate through selectors
     @parserObj.each_selector() do |selector, declarations, specificity|
         if  declarations =~ /#{cssPropertyMask}/ 
           #just set the selector as key and 1 to avoid duplicate elements if we use a hash
           cssSelectors["#{selector}"] = 1
-  
+
           logger ({"type"     => 'info',
                   "function" => "#{functionName}", 
                   "message"  => "true! #{selector} -> #{cssSelectors["#{selector}"]}"})
         end
     end
-  
+
     # get selector into array
     resultingSelectors = Array.new(cssSelectors.keys)
-  
+
     logger ({"type"     => 'info',
             "function" => "#{functionName}", 
             "message"  => "Resulting Selectors:"})
-  
+
     resultingSelectors.each do | selector |
       logger ({"type" => 'info', "function" => "#{functionName}", "message"  => "#{selector}"})     
     end
-  
+
     return resultingSelectors
-  
+
   end #getSelectors
+
+
+
+  def getAllSelectorsInDirectory (params)
+    #look for needed parameters or return
+    for parameter in ['directory', 'cssPropertyMask']
+      if !params["#{parameter}"]
+        logger ({"type"     => 'error', 
+                "function" => 'getAllSelectorsInDirectory', 
+                "message"  => "needed #{parameter} parameter!"})
+        return false
+      end
+    end
+    
+    # get all files in directory
+    fileList = Hash.new
+    logger ({"type"     => 'info',
+             "function" => 'getAllSelectorsInDirectory', 
+             "message"  => "File List of dir: #{params['directory']}"})
+
+    dirEntries = Array.new(Dir.entries("#{params['directory']}"))
+    dirEntries.each do | file |
+      if isValidFile("file" => "#{params["directory"]}\/#{file}") 
+         fileList["#{file}"] = 1;
+      end
+    end
+
+    allSelectors = Hash.new
+
+    # make the search for every file
+    for file in fileList.keys
+
+      fileResults = Hash.new
+
+      fileResults = getSelectors({
+          "cssPropertyMask" => "#{params['cssPropertyMask']}",
+          "cssFile"         => "#{params["directory"]}\/#{file}" })
+      
+      fileResults.each do |selector|
+        allSelectors["#{selector}"] = 1 
+      end
+
+    end
+
+    return allSelectors.keys
+  end # getAllSelectorsInDirectory
 
   # ---
   # private functions
   # ---
   
-  # private 
-  
+  # private
+
+
+
   def isValidFile (params)
-  
+
     #look for needed parameters or return
     for parameter in ['file']
       if !params["#{parameter}"]
@@ -87,21 +135,36 @@ class CssSearch
     end
 
     if !File.exists?("#{params["file"]}")
-      logger ({"type"     => 'error', 
-              "function" => 'getProperFile', 
+      logger ({"type"    => 'error', 
+              "function" => 'isValidFile', 
               "message"  => "file doesn't exists: #{params["file"]}"})      
       return false
     end
 
-    if ! /\.css\Z/ =~ "#{params["file"]}"
-      logger ({"type"     => 'info', 
+    if File.directory?("#{params["file"]}")
+      logger ({"type"    => 'info', 
               "function" => 'isValidFile', 
-              "message"  => "this is not a valid css file"})
+              "message"  => "#{params["file"]} is a directory"})      
       return false
     end
-    
+
+
+    if !(/\.css\Z/ =~ "#{params["file"]}")
+      logger ({"type"    => 'info', 
+              "function" => 'isValidFile', 
+              "message"  => "#{params["file"]} is not a valid css file"})
+      return false
+    end
+
+    logger ({"type"     => 'info', 
+            "function" => 'isValidFile', 
+            "message"  => "Valid!!: #{params["file"]}"})
+
     return true
   end # isValidFile
+
+
+
 
   def logger (params)
     # print message just if debug is enabled
@@ -128,5 +191,7 @@ class CssSearch
     puts "#{prefix["#{params['type']}"]} - #{params['function']}: #{params['message']}"
 
   end # loggers
+
+
 
 end #class
--- css_search/test/cssSearch_test.rb
+++ css_search/test/cssSearch_test.rb
@@ -14,7 +14,7 @@ class TestCssSearch < Test::Unit::TestCase
   # create and prepare the CssParser object
   def setup
     @params = Hash.new
-    @params["debug"] = true
+    @params["debug"] = false
     @cssSearchObj = CssSearch.new(@params)
   end
 
@@ -30,7 +30,7 @@ class TestCssSearch < Test::Unit::TestCase
     results[2] = "p.element-with-property"
     results[3] = "span"
 
-    assert_equal results, @cssSearchObj.getSelectors(@params), "getSelectors for border-radius test!"
+    assert_equal results, @cssSearchObj.getSelectors(@params), "getSelectors for border-radius!"
   end
 
   def test_isValidFile
@@ -38,11 +38,11 @@ class TestCssSearch < Test::Unit::TestCase
     assert_equal true, @cssSearchObj.isValidFile(@params)
   end
 
-  def test_getSelectorsInFolder
+  def test_getAllSelectorsInDirectory
     @params["cssPropertyMask"] = 'border-radius'
-    @params["path"] = './resources/'
-  
-    results = Array.new()
+    @params["directory"] = './resources/'
+
+    results = Array.new
     results[0] = "#id-with-property"
     results[1] = ".class-with-property"
     results[2] = "p.element-with-property"
@@ -51,8 +51,8 @@ class TestCssSearch < Test::Unit::TestCase
     results[5] = ".class-with-property.example2"
     results[6] = "p.element-with-property.example2"
     results[7] = "span.example2"
-  
-    assert_equal results, @cssSearchObj.getAllSelectorsInFolder(@params), "getSelectors all selectors in folder for border-radius test!"
+
+    assert_equal results, @cssSearchObj.getAllSelectorsInDirectory(@params), "getSelectors all selectors with border-radius in all files in given directory!"
   end
-  
+
 end
